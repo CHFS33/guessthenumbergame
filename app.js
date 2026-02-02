8da2739a0a31aec9ea94cc56471e0cb79bddd014
Christopher <chrishhfsmith@gmail.com>
2026-02-02 15:40:21 -0600

    defined the main game values, added basic game logic and attached js to css and html

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 "use strict";
 //Game State
+const messageDisplay = document.getElementById("message-text");
+const scoreDisplay = document.getElementById("current-score");
+const roundDisplay = document.getElementById("round-display");
+const totalScoreDisplay = document.getElementById("total-score");
 const userInput = document.getElementById("user-guess");
 const submitBtn = document.getElementById("user-answer");
 let secretNumber = Math.floor(Math.random() * 50) + 1;
+let currentScore = 10;
+let highScore = 0;
 console.log(`Secret Number: ${secretNumber}`);
 //Game Logic
-// const guess = Number(userInput.value);
-// if (!guess) {
-//   messageDisplay.innerText = "Please enter a valid number!";
-//   return;
-// }
-// console.log(guess);
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
   const guess = Number(userInput.value);
-  console.log("Player Guessed:", guess);
+  if (!guess) {
+    messageDisplay.innerText = "⛔ No number inputted!";
+    return;
+  }
+  if (guess === secretNumber) {
+    messageDisplay.innerText = "🎉 Correct! You guessed right!";
+  } else if (guess !== secretNumber) {
+    if (currentScore > 1) {
+      messageDisplay.innerText =
+        guess > secretNumber ?
+          "Secret Number is lower!"
+        : "Secret Number is higher!";
+      currentScore = currentScore - 1;
+      scoreDisplay.innerText = `Current Score ${currentScore}`;
+    } else {
+      messageDisplay.innerText = "You lost this round!";
+      scoreDisplay.innerText = 0;
+    }
+  }
 });
