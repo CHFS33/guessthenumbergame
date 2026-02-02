195d44abc3951d69c6b6b2afb1e29e0943716d41
Christopher <chrishhfsmith@gmail.com>
2026-02-02 14:00:43 -0600

    attached app.js, adjusted some css elements and added evenlistener to button

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 "use strict";
+//Game State
+const userInput = document.getElementById("user-guess");
+const submitBtn = document.getElementById("user-answer");
+let secretNumber = Math.floor(Math.random() * 50) + 1;
+console.log(`Secret Number: ${secretNumber}`);
+//Game Logic
+// const guess = Number(userInput.value);
+// if (!guess) {
+//   messageDisplay.innerText = "Please enter a valid number!";
+//   return;
+// }
+// console.log(guess);
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const guess = Number(userInput.value);
+  console.log("Player Guessed:", guess);
+});
